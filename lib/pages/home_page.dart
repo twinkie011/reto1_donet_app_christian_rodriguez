17043133b3e5c27a7b169ab7c5bce00493e454f9
Christian Gomez <christiangomez844@gmail.com>
2024-09-05 15:16:04 -0600

    añadir texto

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,9 +12,36 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
     appBar: AppBar(
-      backgroundColor: Colors.red,
-      leading: Icon(Icons.menu, color: Colors.red[800],),
+      backgroundColor: Colors.transparent,
+      leading: Padding(padding: const EdgeInsets.only(left:16.0),
+        child: IconButton(icon: Icon(Icons.menu, color: Colors.grey[800],size:36,), onPressed: () {
+          print('boton menu');
+          },)
       ),
+      actions: [ Padding(
+        padding: const EdgeInsets.only(right:16.0),
+        child: IconButton(icon: Icon(Icons.person, color: Colors.grey[800],size:36,), onPressed: () {
+          print('otra cosa');
+          },)
+      ),
+      ]
+      ),
+      body:const Column(children: [
+          Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Row(
+              children: [
+                Text('I want to ',style: TextStyle(fontSize: 32),),
+                 Text('Eat ',style: TextStyle(fontSize: 32,fontWeight: FontWeight.bold, decoration: TextDecoration.underline),),
+              ],
+            ),
+          )
+          //tap bar 
+
+
+          //tap bar view
+
+      ],)
     );
   }
 }
